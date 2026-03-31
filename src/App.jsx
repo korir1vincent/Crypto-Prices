f1ae0818b67980222991abb5e0964b27c6cbaa10
korir1vincent <korirv09@gmail.com>
2026-03-31 15:20:47 +0300

    Cryptocurrencies prices lists

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import {BrowserRouter, Routes, Route} from "react-router";
+import { Home } from "./pages/Home";
+import { CoinDetail } from "./pages/CoinDetail";
+
+function App() {
+
+  
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/coin/:id" element={<CoinDetail />}/>
+    </Routes>
+    </BrowserRouter>
+
+  );
+}
+
+export default App
